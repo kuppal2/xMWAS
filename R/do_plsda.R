76081508929e07b86acb4ec3a7fa5f6ecace2f4c
Karan Uppal <kuppal2@emory.edu>
2017-09-04 15:46:50 -0400

    xMWASv0.4

---
+++ R/do_plsda.R
@@ -0,0 +1,101 @@
+do_plsda <-
+function(X,Y,oscmode="pls",numcomp=3,keepX=15,keepY=15,sparseselect=FALSE,analysismode="regression",pairedanalysis=FALSE,optselect=FALSE,design=NA)
+{
+    repeatmeasures=pairedanalysis
+    
+    #X<-t(X)
+    
+    #Y<-as.numeric(Y)
+    
+    X<-as.data.frame(X)
+    Y<-as.data.frame(Y)
+    #print(Y)
+    classlabels<-Y
+    
+    #opt_comp<-3
+  
+  if(is.na(keepX)==TRUE){
+      
+      keepX=dim(X)[2]
+  }
+  
+  if(is.na(keepY)==TRUE){
+      
+      keepY=dim(Y)[2]
+  }
+  
+
+  
+  
+    if(dim(X)[2]>1){
+        if(optselect==TRUE){
+           
+                    set.seed(123)
+                    opt_comp<-pls.regression.cv(Xtrain=X, Ytrain=Y,  ncomp=c(1:numcomp), nruncv=10, alpha=2/3)
+                
+                if(opt_comp<2){
+                    opt_comp<-2
+                }
+                
+                 keep_x_vec<-rep(keepX,opt_comp)
+                 keep_y_vec<-rep(keepY,opt_comp)
+            
+        }else{
+            opt_comp<-numcomp
+            keep_x_vec<-rep(keepX,opt_comp)
+            keep_y_vec<-rep(keepY,opt_comp)
+        }
+    }
+    
+    
+   
+    if(oscmode=="o1pls"){
+        leukemia.pls <- plsr(Y ~ X, ncomp = opt_comp, validation = "LOO")
+        ww <- leukemia.pls$loading.weights[,1]
+        pp <- leukemia.pls$loadings[,1]
+        w.ortho <- pp - crossprod(ww, pp)/crossprod(ww) * ww
+        t.ortho <- X %*% w.ortho
+        
+        p.ortho <- crossprod(X, t.ortho) / c(crossprod(t.ortho))
+        Xcorr <- X - tcrossprod(t.ortho, p.ortho)
+        
+        
+        X<-Xcorr
+    }
+ 
+    bad_variables<-{}
+    
+    if(sparseselect==TRUE)
+    {
+            if(repeatmeasures==TRUE){
+                
+                print("design")
+                print(design)
+                linn.pls <- try(multilevel(X=X, design=design,ncomp = opt_comp,
+                keepX = keep_x_vec, Y=Y,keepY=keep_y_vec,method = 'spls',mode=analysismode),silent=TRUE)
+                
+                if(is(linn.pls,"try-error")){
+                    
+                    save(Y,file="Y.Rda")
+                    save(design,file="design.Rda")
+                    linn.pls <- spls(X, Y,ncomp=opt_comp,keepX=keep_x_vec,keepY=keep_y_vec,mode=analysismode,multilevel=design)
+                    
+                }
+                
+               
+                save(linn.pls,file="linn_pls.Rda")
+
+            }else{
+                linn.pls <- spls(X, Y,ncomp=opt_comp,keepX=keep_x_vec,keepY=keep_y_vec,mode=analysismode)
+            }
+     
+        
+    }else{
+        
+                linn.pls <- pls(X, Y,ncomp=opt_comp,mode=analysismode)
+    }
+    
+    #save(linn.pls,file="pls_res.Rda")
+    return(list("model"=linn.pls,"opt_comp"=opt_comp))
+    
+}
