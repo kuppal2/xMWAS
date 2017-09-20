941aeb1c35c4f381893c49007e8210014dd1fce9
Karan Uppal <kuppal2@emory.edu>
2017-09-19 21:29:24 -0400

    xMWASv0.44

---
+++ example_manual_tutorial/example_xmwas_runscript_v0.4.R
@@ -0,0 +1,27 @@
+#load package
+library(xMWAS)
+
+#example dataset that includes metabolome, transcriptome, and cytokine data from the H1N1 mice study (Chandler 2016)
+data(exh1n1)
+data(classlabels_casecontrol) #example classlabels file for case vs control design
+data(classlabels_repeatmeasures) #example classlabels file for repeat measures design
+xMat<-exh1n1$metabolome
+yMat<-exh1n1$transcrlptome #or exh1n1$transcriptome
+zMat<-exh1n1$cytokine
+classlabels<-exh1n1$classlabels
+
+output<-"/Users/karanuppal/Downloads/INPUT_XMwas_1/joshCd/testv0.4/"  #change for your computer
+
+#Please see user manual for description of arguments:
+#https://sourceforge.net/projects/xmwas/files/xMWAS-manual.pdf/download
+
+#call the run_xmwas() function:
+xmwas_res<-run_xmwas(Xome_data=xMat,Yome_data=yMat,Zome_data=zMat,Wome_data=NA,outloc=output,
+classlabels=classlabels,class_fname=NA,xmwasmethod="spls",plsmode="canonical",max_xvar=5000,max_yvar=5000,
+max_zvar=5000,max_wvar=5000,rsd.filt.thresh=1,corthresh=0.3,keepX=100,keepY=100,keepZ=100,keepW=100,
+pairedanalysis=FALSE,optselect=TRUE,rawPthresh=0.05,numcomps=10,net_edge_colors=c("blue","red"),
+net_node_colors=c("orange", "green","cyan","gold"),Xname="X",Yname="Y",Zname="Z",Wname="W",
+net_node_shape=c("rectangle","circle","triangle","star"),all.missing.thresh=0.3,
+seednum=100,label.cex=0.2,vertex.size=6,graphclustering=TRUE,interactive=FALSE,max_connections=10000,
+centrality_method="eigenvector",use.X.reference=FALSE,removeRda=TRUE)
+suppressWarnings(try(sink(file=NULL),silent=TRUE))
