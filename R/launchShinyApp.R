baec971107976ef2a070d8dc40cbaed62fc10c1b
Karan Uppal <kuppal2@emory.edu>
2019-07-24 09:07:32 -0400

    remove all files

--- R/launchShinyApp.R
+++
@@ -1,9 +0,0 @@
-launchShinyApp <-
-function() {
-    appDir <- system.file("shinyapp", package = "xMWAS")
-    if (appDir == "") {
-        stop("Could not find shinyapp directory. Try re-installing `xMWAS`.", call. = FALSE)
-    }
-    
-    shiny::runApp(appDir, display.mode = "normal")
-}
--- inst/shinyapp/R/communitydetection.R
+++
@@ -1,5 +0,0 @@
-library(shiny)
-
-communitydetection<-fluidRow(
-           column(6,selectInput("centrality_method","Method for centrality analysis:",c("eigenvector","betweenness","degree.count","degree.weight","closeness")))
-)
