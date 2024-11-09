73b782b66fafbd2c57a7500785ad71203caa0cbd
kuppal3gt <kuppal3gt@gmail.com>
2024-11-09 18:11:50 -0500

    v1.0 - updated for R 4.0

--- inst/shinyapp/ui.R
+++ inst/shinyapp/ui.R
@@ -8,10 +8,10 @@ ui<-fluidPage(
     tags$head(
       tags$meta(charset="utf-8"),
       tags$meta(name="description",content="Free Web tutorials"),
-      tags$title("xMWAS - v0.56"),
+      tags$title("xMWAS - v1.0"),
       tags$link(rel = "stylesheet", type = "text/css", href = "mystyle.css")
     ),
-    column(12,tags$div(headerPanel(h3("xMWAS - a data-driven integration and network analysis tool (v0.56)")))),
+    column(12,tags$div(headerPanel(h3("xMWAS - a data-driven integration and network analysis tool (v1.0)")))),
     column(12,tabsetPanel(
         tabPanel("Introduction", introduction_page), 
         tabPanel("Analysis", analysis_page), 
@@ -20,8 +20,8 @@ ui<-fluidPage(
       )),
     column(style="padding-top:0px;padding-bottom:0px;",12,tags$hr(style="margin-top:0px;margin-bottom:15px;border-top: 0.5px solid #ccccb3;")),
     column(12,  tags$div(style="margin-center",tags$footer(align="center",color="white",style="font-weight:normal;font-size:80%;color:black","Citation: Uppal K, Ma C, Go YM, Jones DP. xMWAS: a data-driven integration and differential network analysis tool. ",tags$span(style='font-style:italic;','Bioinformatics.')," 2018 Feb 15. PMID: ",tags$a(href="https://www.ncbi.nlm.nih.gov/pubmed/29069296",target="_blank","29069296")))),
- #column(12,  tags$div(style="margin-center",tags$footer(align="center",color="white",style="font-weight:normal;font-size:80%;color:black","Maintained by Chunyu Ma (",tags$a(href="mailto:chunyu.ma@emory.edu","chunyu.ma@emory.edu"),") and Karan Uppal (",tags$a(href="mailto:kuppal2@emory.edu","kuppal2@emory.edu"),") at ",tags$a(href="http://clinicalmetabolomics.org/init/default/index",target="_blank","Clinical Biomarkers Laboratory"),", Emory University, Atlanta, GA, USA")))
+ #column(12,  tags$div(style="margin-center",tags$footer(align="center",color="white",style="font-weight:normal;font-size:80%;color:black","Maintained by Karan Uppal (",tags$a(href="mailto:kuppal3gt@gmail.com","kuppal3gt@gmail.com"),"))
  column(12,  tags$div(style="margin-center",tags$footer(align="center",color="white",style="font-weight:normal;font-size:95%;color:black","Please ask questions or report any issues on the ",
                                                         tags$a(href='https://github.com/kuppal2/xMWAS/issues',target="_blank","GitHub"), " page"))),
- column(12,  tags$div(style="margin-center",tags$footer(align="center",color="white",style="font-weight:normal;font-size:95%;color:black","Release date: 03/31/2021")))
+ column(12,  tags$div(style="margin-center",tags$footer(align="center",color="white",style="font-weight:normal;font-size:95%;color:black","Release date: 11/09/2024")))
  )
